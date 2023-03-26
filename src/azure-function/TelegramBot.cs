19baa0e0fb8fa4daa269b0c7221f70bffcbb9804
Leonid Petrov1 <leonid_petrov1@epam.com>
2023-03-27 00:46:30 +0300

    Migrate chatGpt logic from worker

--- src/azure-function/TelegramBot.cs
+++ src/azure-function/TelegramBot.cs
@@ -1,36 +1,49 @@
-using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
 
 namespace azure_function
 {
     public class TelegramBot
     {
         private readonly ILogger _logger;
+        private readonly UpdateService _updateService;
 
-        public TelegramBot(ILoggerFactory loggerFactory)
+        public TelegramBot(ILoggerFactory loggerFactory, UpdateService updateService)
         {
             _logger = loggerFactory.CreateLogger<TelegramBot>();
+            _updateService = updateService;
         }
 
         [Function("TelegramBot")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req,
+            CancellationToken cancellation)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            var tgToken = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN", EnvironmentVariableTarget.Process)
-                ?? throw new ArgumentException("TELEGRAM_BOT_TOKEN is missing");
-            var openAiApiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY", EnvironmentVariableTarget.Process)
-                ?? throw new ArgumentException("OPENAI_API_KEY is missing");
-            var openAiOrg = Environment.GetEnvironmentVariable("OPENAI_ORG", EnvironmentVariableTarget.Process)
-                ?? throw new ArgumentException("OPENAI_ORG is missing");
-
-
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            try
+            {
+                var body = await req.ReadAsStringAsync() ?? throw new ArgumentNullException(nameof(req));
+                var update = JsonConvert.DeserializeObject<Update>(body);
+                if (update is null)
+                {
+                    _logger.LogWarning("Unable to deserialize Update object.");
+                    return response;
+                }
 
-            response.WriteString("Welcome to Azure Functions!");
+                await _updateService.Handle(update, cancellation);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Exception: {exception}", e.Message);
+            }
 
             return response;
         }
