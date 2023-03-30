7ba715ad2e23f70691e71ad7986bbb289ed7060f
Leonid Petrov1 <leonid_petrov1@epam.com>
2023-03-31 02:52:08 +0300

    Convert to file-scoped namespaces

--- src/azure-function/TelegramBot.cs
+++ src/azure-function/TelegramBot.cs
@@ -8,44 +8,43 @@ using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot.Types;
 
-namespace azure_function
+namespace azure_function;
+
+public class TelegramBot
 {
-    public class TelegramBot
+    private readonly ILogger _logger;
+    private readonly UpdateService _updateService;
+
+    public TelegramBot(ILoggerFactory loggerFactory, UpdateService updateService)
     {
-        private readonly ILogger _logger;
-        private readonly UpdateService _updateService;
+        _logger = loggerFactory.CreateLogger<TelegramBot>();
+        _updateService = updateService;
+    }
 
-        public TelegramBot(ILoggerFactory loggerFactory, UpdateService updateService)
+    [Function("TelegramBot")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req,
+        CancellationToken cancellation)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        try
         {
-            _logger = loggerFactory.CreateLogger<TelegramBot>();
-            _updateService = updateService;
-        }
-
-        [Function("TelegramBot")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req,
-            CancellationToken cancellation)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-            try
+            var body = await req.ReadAsStringAsync() ?? throw new ArgumentNullException(nameof(req));
+            var update = JsonConvert.DeserializeObject<Update>(body);
+            if (update is null)
             {
-                var body = await req.ReadAsStringAsync() ?? throw new ArgumentNullException(nameof(req));
-                var update = JsonConvert.DeserializeObject<Update>(body);
-                if (update is null)
-                {
-                    _logger.LogWarning("Unable to deserialize Update object.");
-                    return response;
-                }
-
-                await _updateService.Handle(update, cancellation);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError("Exception: {exception}", e.Message);
+                _logger.LogWarning("Unable to deserialize Update object.");
+                return response;
             }
 
-            return response;
+            await _updateService.Handle(update, cancellation);
         }
+        catch (Exception e)
+        {
+            _logger.LogError("Exception: {exception}", e.Message);
+        }
+
+        return response;
     }
 }
--- src/azure-function/UpdateService.cs
+++ src/azure-function/UpdateService.cs
@@ -10,126 +10,125 @@ using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
-namespace azure_function
+namespace azure_function;
+
+public class UpdateService
 {
-    public class UpdateService
+    private const long OurChatGroupId = -1001034436662;
+
+    private readonly ITelegramBotClient _botClient;
+    private readonly ILogger<UpdateService> _logger;
+    private readonly IOpenAIService _openAiService;
+
+    public UpdateService(ITelegramBotClient botClient, ILogger<UpdateService> logger, IOpenAIService openAiService)
     {
-        private const long OurChatGroupId = -1001034436662;
+        _botClient = botClient;
+        _logger = logger;
+        _openAiService = openAiService;
+    }
 
-        private readonly ITelegramBotClient _botClient;
-        private readonly ILogger<UpdateService> _logger;
-        private readonly IOpenAIService _openAiService;
+    public async Task Handle(Update update, CancellationToken cancellation)
+    {
+        _logger.LogInformation("Invoke telegram update function");
 
-        public UpdateService(ITelegramBotClient botClient, ILogger<UpdateService> logger, IOpenAIService openAiService)
+        var handler = update switch
         {
-            _botClient = botClient;
-            _logger = logger;
-            _openAiService = openAiService;
-        }
+            // UpdateType.Unknown:
+            // UpdateType.ChannelPost:
+            // UpdateType.EditedChannelPost:
+            // UpdateType.ShippingQuery:
+            // UpdateType.PreCheckoutQuery:
+            // UpdateType.Poll:
+            { Message: { } message } => BotOnMessageReceived(message, cancellation),
+            { EditedMessage: { } message } => BotOnMessageReceived(message, cancellation),
+            _ => UnknownUpdateHandlerAsync(update, cancellation)
+        };
 
-        public async Task Handle(Update update, CancellationToken cancellation)
+        await handler;
+    }
+
+    private async Task BotOnMessageReceived(Message message, CancellationToken cancellation)
+    {
+        _logger.LogInformation("Receive message type: {MessageType}. Group: {GroupId}", message.Type, message.Chat.Id);
+        if (message.Text is not { } messageText)
+            return;
+
+        if (message.Chat.Id != OurChatGroupId)
         {
-            _logger.LogInformation("Invoke telegram update function");
-
-            var handler = update switch
-            {
-                // UpdateType.Unknown:
-                // UpdateType.ChannelPost:
-                // UpdateType.EditedChannelPost:
-                // UpdateType.ShippingQuery:
-                // UpdateType.PreCheckoutQuery:
-                // UpdateType.Poll:
-                { Message: { } message } => BotOnMessageReceived(message, cancellation),
-                { EditedMessage: { } message } => BotOnMessageReceived(message, cancellation),
-                _ => UnknownUpdateHandlerAsync(update, cancellation)
-            };
-
-            await handler;
-        }
-
-        private async Task BotOnMessageReceived(Message message, CancellationToken cancellation)
-        {
-            _logger.LogInformation("Receive message type: {MessageType}. Group: {GroupId}", message.Type, message.Chat.Id);
-            if (message.Text is not { } messageText)
-                return;
-
-            if (message.Chat.Id != OurChatGroupId)
-            {
-                await _botClient.SendTextMessageAsync(
-                    chatId: message.Chat.Id,
-                    text: "Only for my chat.",
-                    replyToMessageId: message.MessageId,
-                    cancellationToken: cancellation);
-                return;
-            }
-
-            bool containMention = message.Entities?.Any(x => x.Type == MessageEntityType.Mention) ?? false;
-            bool isBotMentioned = containMention && (message.EntityValues?.Any(x => x.Equals("@chatgpt_ungerfall_bot")) ?? false);
-            if (!isBotMentioned)
-            {
-                _logger.LogInformation("The message does not contain mention of bot.");
-                return;
-            }
-
-            await _botClient.SendChatActionAsync(
+            await _botClient.SendTextMessageAsync(
                 chatId: message.Chat.Id,
-                ChatAction.Typing,
-                cancellationToken: cancellation);
-            var chatGptResponse = await SendChatGptMessage(message.Text, message.From?.Username ?? "unknown", cancellation);
-            Message sentMessage = await _botClient.SendTextMessageAsync(
-                chatId: message.Chat.Id,
-                text: chatGptResponse,
+                text: "Only for my chat.",
                 replyToMessageId: message.MessageId,
                 cancellationToken: cancellation);
-
-            _logger.LogInformation("The message was sent with id: {SentMessageId}", sentMessage.MessageId);
+            return;
         }
 
-        private async Task<string> SendChatGptMessage(string message, string user, CancellationToken cancellation)
+        bool containMention = message.Entities?.Any(x => x.Type == MessageEntityType.Mention) ?? false;
+        bool isBotMentioned = containMention && (message.EntityValues?.Any(x => x.Equals("@chatgpt_ungerfall_bot")) ?? false);
+        if (!isBotMentioned)
         {
-            _logger.LogInformation("Sending {Message} from {User}", message, user);
+            _logger.LogInformation("The message does not contain mention of bot.");
+            return;
+        }
 
-            var completionResult = await _openAiService.ChatCompletion.CreateCompletion(
-                new ChatCompletionCreateRequest
+        await _botClient.SendChatActionAsync(
+            chatId: message.Chat.Id,
+            ChatAction.Typing,
+            cancellationToken: cancellation);
+        var chatGptResponseTask = SendChatGptMessage(message.Text, message.From?.Username ?? "unknown", cancellation);
+        Message sentMessage = await _botClient.SendTextMessageAsync(
+            chatId: message.Chat.Id,
+            text: await chatGptResponseTask,
+            replyToMessageId: message.MessageId,
+            cancellationToken: cancellation);
+
+        _logger.LogInformation("The message was sent with id: {SentMessageId}", sentMessage.MessageId);
+    }
+
+    private async Task<string> SendChatGptMessage(string message, string user, CancellationToken cancellation)
+    {
+        _logger.LogInformation("Sending {Message} from {User}", message, user);
+
+        var completionResult = await _openAiService.ChatCompletion.CreateCompletion(
+            new ChatCompletionCreateRequest
+            {
+                Messages = new[]
                 {
-                    Messages = new[]
-                    {
-                        ChatMessage.FromSystem("You are an AI that provides brief and concise answers."),
-                        ChatMessage.FromUser(message),
-                    },
-                    Temperature = 0f,
-                    User = user,
+                    ChatMessage.FromSystem("You are an AI that provides brief and concise answers."),
+                    ChatMessage.FromUser(message),
                 },
-                cancellationToken: cancellation);
-            if (completionResult.Successful)
-            {
-                return completionResult.Choices[0].Message.Content;
-            }
-            else
-            {
-                return "ChatGPT request wasn't successful";
-            }
-        }
-
-        private Task UnknownUpdateHandlerAsync(Update update, CancellationToken _)
+                Temperature = 0f,
+                User = user,
+            },
+            cancellationToken: cancellation);
+        if (completionResult.Successful)
         {
-            _logger.LogInformation("Unknown update type: {UpdateType}", update.Type);
-            return Task.CompletedTask;
+            return completionResult.Choices[0].Message.Content;
         }
-
-        public async Task HandlePollingErrorAsync(Exception exception, CancellationToken cancellationToken)
+        else
         {
-            var ErrorMessage = exception switch
-            {
-                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-                _ => exception.ToString()
-            };
-
-            _logger.LogInformation("HandleError: {ErrorMessage}", ErrorMessage);
-
-            // Cooldown in case of network connection error
-            if (exception is RequestException)
-                await Task.Delay(TimeSpan.FromSeconds(2), cancellationToken);
+            return "ChatGPT request wasn't successful";
         }
+    }
+
+    private Task UnknownUpdateHandlerAsync(Update update, CancellationToken _)
+    {
+        _logger.LogInformation("Unknown update type: {UpdateType}", update.Type);
+        return Task.CompletedTask;
+    }
+
+    public async Task HandlePollingErrorAsync(Exception exception, CancellationToken cancellationToken)
+    {
+        var ErrorMessage = exception switch
+        {
+            ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        _logger.LogInformation("HandleError: {ErrorMessage}", ErrorMessage);
+
+        // Cooldown in case of network connection error
+        if (exception is RequestException)
+            await Task.Delay(TimeSpan.FromSeconds(2), cancellationToken);
     }
 }
