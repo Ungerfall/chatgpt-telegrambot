39b0ff56a297cbeaaf03e9e17265008ce87a2ce4
Leonid Petrov1 <leonid_petrov1@epam.com>
2023-07-02 23:12:57 +0300

    WIP add prompts configuration

---
+++ src/application/Configuration/ChatGptPrompt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ungerfall.ChatGpt.TelegramBot.Configuration;
+public class ChatGptPrompt
+{
+    /// <summary>
+    /// Number of tokens for historical messages in a request.
+    /// </summary>
+    [Required]
+    [Range(1d, 1024 * 4)]
+    public int Tokens { get; set; }
+
+    [Required]
+    public string Prompt { get; set; } = null!;
+}
