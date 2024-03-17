108de945543d85355ffb8fecd0abfdea29fdc904
Leonid Petrov1 <leonid_petrov1@epam.com>
2024-03-17 05:34:58 +0300

    Force dotnet 8.0 on publish

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,5 +1,6 @@
 $additionalParams = $args -join ' '
-$command = "func azure functionapp publish fapp-ungerfall-chatgpt-telegram-webhook --show-keys $additionalParams"
+$dotnet = "8.0"
+$command = "func azure functionapp publish fapp-ungerfall-chatgpt-telegram-webhook --show-keys --dotnet-version $dotnet $additionalParams"
 
 cd .\src\presentation.azure-function\
 Invoke-Expression $command
